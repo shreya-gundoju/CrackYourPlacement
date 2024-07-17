9f0920013e81ba701e048f8b8087b3b7d3cee91e
Shreya <131763902+shreya-gundoju@users.noreply.github.com>
2024-07-17 11:36:27 +0530

    Update set-matrix-zeroes.java

--- Day2/set-matrix-zeroes.java
+++ Day2/set-matrix-zeroes.java
@@ -1,26 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int r=matrix.length,c=matrix[0].length;
-        boolean[] rarr = new boolean[r];
-        boolean[] carr = new boolean[c];
+        int[] rarr = new int[r];
+        Arrays.fill(rarr, 0);
+        int[] carr = new int[c];
+        Arrays.fill(carr, 0);
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(matrix[i][j]==0){
-                    rarr[i]=true;
-                    carr[j]=true;
+                    rarr[i]=1;
+                    carr[j]=1;
                 }
             }
         }
         for(int i = 0; i < r; ++i){
-            if(rarr[i]){
-                for(int j = 0; j < c; ++j){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for(int j = 0; j < c; ++j){
-            if(carr[j]){
-                for(int i = 0; i < r; ++i){
+            for(int j = 0; j < c; ++j){
+                if(rarr[i]==1 || carr[j]==1){
                     matrix[i][j] = 0;
                 }
             }
